20f3326a10befeb7ab692c44b225e55e482c9821
johann <johann.chipan1@gmail.com>
2025-03-05 14:23:31 +0100

    Models

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,11 +1,24 @@
 from sqlalchemy import Column, Integer, String
 from app.database import Base
+from datetime import datetime, timezone
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+    last_name = Column(String, nullable=True)
     email = Column(String, unique=True, index=True)
     first_name = Column(String, nullable=True)
-    last_name = Column(String, nullable=True)
+    password = Column(String, nullable=True)
     
+    recettes = relationship("Recette", back_populates="user")
+    comments = relationship("Comment", back_populates="user")
+
+class Recette(Base):
+    __tablename__ = "recettes"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    titre = Column(String, nullable=True)
+    description = Column(String, nullable=True)
+    instructions = Column(String, nullable=True)
+    created_at = Column(datetime, default=datetime.now(timezone.utc))
