df82272fcd73b0c13bef20b3f594959f5ea102dc
Anthony <=>
2025-03-05 17:13:17 +0100

    relations

--- backend/app/models/recette.py
+++ backend/app/models/recette.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
-from datetime import datetime, timezone
+from datetime import timezone, datetime
 
 class Recette(Base):
     __tablename__ = "Recettes"
@@ -10,11 +10,15 @@ class Recette(Base):
     titre = Column(String, nullable=True)
     description = Column(String, nullable=True)
     instructions = Column(String, nullable=True)
-    created_at = Column(datetime, default=datetime.now(timezone.utc))
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+    user_id = Column(Integer, ForeignKey('Users.id'), nullable=False)
 
-    # user = relationship("User", back_populates="Recette")
-    commentaires = relationship("Commentaire", back_populates="Recette")
-    favories = relationship("Favorie", back_populates="Recette")
-    recettes_ingrédients = relationship("Recette_ingredient", back_populates="Recette")
+    user = relationship("User", back_populates="recettes")
+    commentaires = relationship("Commentaire", back_populates="recipe")
+    favories = relationship("Favorie", back_populates="recette")
+    recettes_ingredients = relationship("Recette_ingredient", back_populates="recette")
 
-
+from app.models.user import User
+from app.models.commentaires import Commentaire
+from app.models.favorie import Favorie
+from app.models.recette_ingredient import Recette_ingredient
--- backend/app/models/recette_ingredient.py
+++ backend/app/models/recette_ingredient.py
@@ -5,9 +5,12 @@ from app.database import Base
 class Recette_ingredient(Base):
     __tablename__ = 'Recettes_ingredients'
     
-    recette_id = Column(Integer, ForeignKey('Recette.id'), primary_key=True)
+    recette_id = Column(Integer, ForeignKey('Recettes.id'), primary_key=True)
     ingredient_id = Column(Integer, ForeignKey('Ingredients.id'), primary_key=True)
     quantity = Column(Integer, nullable=False)
     
-    recette = relationship("Recette", back_populates="Ingredient")
-    ingredient = relationship("Ingredient", back_populates="Recette")
+    recette = relationship("Recette", back_populates="recettes_ingredients")
+    ingredient = relationship("Ingredient", back_populates="recette")
+
+from app.models.recette import Recette
+from app.models.ingredient import Ingredient
